d9cdcd6032f528e081f36c96515365ae23dde526
Andrii <shaposhnikandriy@gmail.com>
2018-05-08 21:21:34 +0300

    WhiteFactory added

---
+++ src/WhiteFactory.java
@@ -0,0 +1,12 @@
+
+public class WhiteFactory implements BaseFactory {
+    @Override
+    public Circle getCircle(double radius) {
+        return null;
+    }
+
+    @Override
+    public Triangle getTriangle(double a, double b, double c) {
+        return null;
+    }
+}
