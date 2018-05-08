46306c8e1582d337dec301929757402570057bc8
AntonYevtukh <anton.yevtukh@gmail.com>
2018-05-08 17:48:49 +0300

    AbstractFactory and BaseFactory has been added, Runner has been modified

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,24 @@
 public class Runner {
     public static void main(String[] args) {
-        
+
+        try {
+            BaseFactory blackFactory = AbstractFactory.getFactory("black");
+            
+            Triangle blackTriangle = blackFactory.getTriangle(3, 4, 5);
+            System.out.println(blackTriangle);
+            Circle blackCircle = blackFactory.getCircle(10);
+            System.out.println(blackCircle);
+            
+            BaseFactory whiteFactory = AbstractFactory.getFactory("white");
+
+            Triangle whiteTriangle = whiteFactory.getTriangle(3, 4, 5);
+            System.out.println(whiteTriangle);
+            Circle whiteCircle = whiteFactory.getCircle(10);
+            System.out.println(whiteCircle);
+
+
+        } catch (RuntimeException e) {
+            System.out.println("Factory not found for specified color");
+        }
     }
 }
