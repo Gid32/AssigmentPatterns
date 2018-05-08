75a6b8a07c363a662e1bf2fdf3cce89a3a36f4fd
r.stetsenko@gmail.com <PrisonBreak1982>
2018-05-08 21:09:33 +0300

    black factory

--- src/BlackFactory.java
+++ src/BlackFactory.java
@@ -1,4 +1,4 @@
-public class BlackFactory extends BaseFactory {
+public class BlackFactory implements BaseFactory {
     private BaseFactory baseFactory;
     private static final String COLOR = "black";
 
@@ -6,17 +6,19 @@ public class BlackFactory extends BaseFactory {
         this.baseFactory = baseFactory;
     }
 
-    public void createBlackTriangle(double side1, double side2, double side3) {
+    public BlackTriangle createBlackTriangle(double side1, double side2, double side3) {
         BlackTriangle blackTriangle = (BlackTriangle) baseFactory.createTriangle();
         blackTriangle.setSide1(side1);
         blackTriangle.setSide2(side2);
         blackTriangle.setSide3(side3);
         blackTriangle.setColor(COLOR);
+        return blackTriangle;
     }
 
-    public void createBlackCircle(double radius) {
+    public BlackCircle createBlackCircle(double radius) {
         BlackCircle blackCircle = (BlackCircle) baseFactory.createCircle();
         blackCircle.setRadius(radius);
         blackCircle.setColor(COLOR);
+        return blackCircle;
     }
 }
