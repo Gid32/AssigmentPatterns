15e533c0a105ab310bef6916e808039f5c43895e
Andrii <shaposhnikandriy@gmail.com>
2018-05-10 18:57:29 +0300

    new White Factory

--- src/WhiteFactory.java
+++ src/WhiteFactory.java
@@ -1,12 +1,13 @@
+import Circle.WhiteCircle;
 
-public class WhiteFactory implements BaseFactory {
+public class WhiteFactory extends BaseFactory {
     @Override
-    public Circle getCircle(double radius) {
-        return null;
+    public Circle createCircle(double radius) {
+        return new WhiteCircle(radius);
     }
 
     @Override
-    public Triangle getTriangle(double a, double b, double c) {
-        return null;
+    public Triangle createTriangle(double a, double b, double c) {
+        return new WhiteTriangle(a, b, c);
     }
 }
